2fad532a10cfef5d60525b7902f72d6386ff6617
shreeniv <69985129+shreeniv@users.noreply.github.com>
2021-02-01 22:05:34 -0500

    another

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import psycopg2
 import numpy as np
 import socket
-import SQLAlchemy from flask_sqlalchemy
+#import SQLAlchemy from flask_sqlalchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
